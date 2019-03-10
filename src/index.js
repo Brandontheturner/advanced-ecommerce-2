350862c2287c1555a175c3fb2914683f2b5b6753
Brandon Turner <brandontheturner@gmail.com>
2019-03-09 18:04:22 -0600

    working on currentCategory function

--- src/index.js
+++ src/index.js
@@ -4,6 +4,14 @@ import App from "./App";
 import "./index.css";
 import state from "./state";
 
+let currentCategory = "tires";
+currentCategory = (cat) => {
+  currentCategory().category then rener
+  //assign category to currentCategory then call render()
+} 
+//set currentCategory to tires?? 
+currentCategory = tires
+
 function render() {
   ReactDOM.render(
     <App state={state} />,
