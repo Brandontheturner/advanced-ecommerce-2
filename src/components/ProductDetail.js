1b2cca7426f428f87573d013f25e8ce85f4565e5
Brandon Turner <brandontheturner@gmail.com>
2019-03-12 21:29:06 -0500

    trying to figure out props and proptypes

--- src/components/ProductDetail.js
+++ src/components/ProductDetail.js
@@ -1,26 +1,65 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
-import State from "../state";
 
 const ProductDetail = props => {
-  // render() {
   return (
     <div>
       <div className="grid_1_of_5 images_1_of_5">
-        <img src>{props.imgUrl}</img>
-        <h3>{props.product}</h3>
-        <p>{props.description}</p>
-        <h4>{props.price}</h4>
+        <img src="images/g1.jpg" />
+        <h3>Lorem Ipsum is simply </h3>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit, in
+          reprehenderit.
+        </p>
+        <h4>$300.00</h4>
         <div className="button">
           <span>
             <a href="singlepage.html">Read More</a>
           </span>
         </div>
       </div>
+      <div className="grid_1_of_5 images_1_of_5">
+        <img src="images/g2.jpg" />
+        <h3>Lorem Ipsum is simply </h3>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit, in
+          reprehenderit.
+        </p>
+        <h4>$120.00</h4>
+        <div className="button">
+          <span>
+            <a href="singlepage.html">Read More</a>
+          </span>
+        </div>
+      </div>
+      <div className="grid_1_of_5 images_1_of_5">
+        <img src="images/g3.png" />
+        <h3>Lorem Ipsum is simply </h3>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit, in
+          reprehenderit.
+        </p>
+        <h4>$500.00</h4>
+        <div className="button">
+          <span>
+            <a href="singlepage.html">Read More</a>
+          </span>
+        </div>
+      </div>
+      <div className="grid_1_of_5 images_1_of_5">
+        <img src="images/g4.jpg" />
+        <h3>Lorem Ipsum is simply</h3>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit, in
+          reprehenderit.
+        </p>
+        <h4>$120.00</h4>
+        <div className="button">
+          <span>
+        </span>
     </div>
   );
 };
-// };
 
 ProductDetail.PropTypes = {
   product: PropTypes.array.isRequired
